b2a4ca51d6b4ca6602c9da70483a2ef6df417bb5
Baktiar Miah <baktiar.m@gmail.com>
2019-09-30 23:12:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,17 +15,15 @@ class Transfer
   
   def execute_transaction
    
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
+    if self.status == 'pending'
+     if sender.balance < amount
+       self.status = "Transaction rejected. Please check your account balance."
+       
+      end 
+      receiver.balance +=amount
+      sender.balance -=amount
+      self.status = 'complete'
+    end 
   
   end 
   
