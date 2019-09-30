456b429d7cc862474692ccc3fb789fbc0b653ad1
Baktiar Miah <baktiar.m@gmail.com>
2019-09-30 22:33:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader: :name
+
+  def initialize(name, balance=1000, status='open')
+    @name = name 
+    @balance = balance 
+    @status = status
+  end 
+  
+  def deposit(money)
+    @balance+=money 
+  end 
+  
+  def display_balance
+    @balance 
+  end 
+  
+  def valid?
+    if @balance > 0 && @status == 'open'
+      true
+    end 
+  end 
+  
+  def close_account
+  end
+
 
 end
